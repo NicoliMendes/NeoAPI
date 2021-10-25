39fbbdb578c32655a63d36a94e8820418b4854f9
michaelctt <michaelgabrielm.c@gmail.com>
2021-10-25 16:15:44 -0300

    Redefinindo layout do login

--- login-app/src/pages/Login/index.js
+++ login-app/src/pages/Login/index.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import './login.css'
+import { MdEmail, MdLock } from "react-icons/md";
 
-const Login = () => {
+function Login() {
     return (
         <div className="login">
-            <h1>Login</h1>
+            <div className="login-logo">
+                <img src="https://cdn-icons-png.flaticon.com/512/3408/3408455.png" 
+                alt="Login App"/>
+            </div>
+
+            <div className="login-right">
+                <h1>Acessar o App</h1>
+
+                <div className="loginInputEmail">
+                    <MdEmail/>
+                    <input 
+                    type="text"
+                    placeholder="Email"
+                    />
+                </div>
+
+                <div className="loginInputPassword">
+                    <MdLock/>
+                    <input 
+                    type="text"
+                    placeholder="Password"
+                    />
+                </div>
+
+            </div>
         </div>
     )
 }
