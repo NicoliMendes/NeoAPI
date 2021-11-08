cfdfbea5a1caacf4510a2b8c34841d59b86e8cb7
M1ch4elG4b <michael.gabriel@mutant.com.br>
2021-11-08 11:38:12 -0300

    Endpoint de login

--- NeomutantAPI/NeomutantAPI/Controllers/LoginController.cs
+++ NeomutantAPI/NeomutantAPI/Controllers/LoginController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,11 +19,36 @@ namespace NeomutantAPI.Controllers
             return View();
 
         }
+
         [HttpPost("Login")] 
    
-        public ActionResult Login()
+        public ActionResult Login(string user, string password)
         {
-            return Ok();
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+            using var con = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            con.Open();
+            string query = $" SELECT COUNT(1) RECORDS FROM Users " +
+                    $" WHERE UserName = '{user}' AND Password = HashBytes('MD5', '{password}')";
+            SqlCommand Consult;
+            Consult = new SqlCommand(query, con);
+            SqlDataReader reader = Consult.ExecuteReader();
+            int NumRecords = 0;
+
+            while (reader.Read())
+                NumRecords = reader.GetInt32(0);
+
+            if (NumRecords > 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
+
         }
     }
 }
