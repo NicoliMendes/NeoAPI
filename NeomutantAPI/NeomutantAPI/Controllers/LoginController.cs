514cc414b21419ba588b4417d26fc409747fae80
NicoliMendes <nicoli.mendes@mutant.com.br>
2021-11-08 09:55:52 -0300

    API

---
+++ NeomutantAPI/NeomutantAPI/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeomutantAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        [HttpPost("Login")] 
+   
+        public ActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
