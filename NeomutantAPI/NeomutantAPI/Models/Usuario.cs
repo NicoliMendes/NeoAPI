4789f524a8360c9c54e4fa4ba84378babd1fa9c2
M1ch4elG4b <michael.gabriel@mutant.com.br>
2021-11-11 09:51:28 -0300

    Model Usuario

---
+++ NeomutantAPI/NeomutantAPI/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeomutantAPI.Models
+{
+	public class Usuario
+	{
+		public int ID_Usuario { get; set; }
+		public string Login { get; set; }
+		public string Senha { get; set; }
+		public string Primeiro_Nome { get; set; }
+		public string Ultimo_Nome { get; set; }
+	}
+}
